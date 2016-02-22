d4039650885e4ed321b20f47f6c9c0485be847bf
NewerLsg <247520226@qq.com>
2016-02-22 23:31:55 +0800

    动态配置
    1.输入框的动态改变
    
    Signed-off-by: NewerLsg <247520226@qq.com>

--- config.py
+++ config.py
@@ -28,6 +28,8 @@ class ConfigDialog(QDialog):
 
 	def __init__(self,parent=None):
 		super(ConfigDialog, self).__init__(parent)
+
+		self.setObjectName(_fromUtf8("ConfigDialog"))
 		self.ui = Ui_Dialog()
 		self.ui.setupUi(self)
 
@@ -38,30 +40,61 @@ class ConfigDialog(QDialog):
 
 		self.gridLayout = [0 for col in range(5)]
 		self.label  	= [0 for col in range(5)]
-		self.LevelEdit  = [[0 for col in range(5)] for row in range(6)]
+		self.LevelEdit  = [0 for col in range(5 * 4)]
+		self.num		= int(0)
 
-
-		for i in xrange(0,4):
-			self.gridLayout[i] = QtGui.QGridLayout()
-			self.gridLayout[i].setObjectName(_fromUtf8("gridLayout_" + str(i)))
-
-			self.label[i] = QtGui.QLabel(self)
-			self.label[i].setObjectName(_fromUtf8("lable_" + str(i)))
-			self.label[i].setText("123")
-			self.gridLayout[i].addWidget(self.label[i], 0, 0, 1, 1)
-
-			for j in xrange(0,5):
-				self.LevelEdit[i][j] = QtGui.QLineEdit(self)
-				self.LevelEdit[i][j].setObjectName(_fromUtf8("Level_0" + str(j) +"_Edit"))
-				self.gridLayout[i].addWidget(self.LevelEdit[i][j], 0, j, 1, 1)
-
-			self.ui.verticalLayout_2.addLayout(self.gridLayout[i])
-
-
-
+		self.ui.comboBox.activated.connect(self.comboBoxActivated)
 		self.ui.CancelBtn.clicked.connect(self.close)
 		self.ui.ResetBtn.clicked.connect(self.reset)
 		self.ui.ComfirmBtn.clicked.connect(self.comfirm)
+
+	def comboBoxActivated(self, num):
+
+		num += 1
+
+		if num == self.num:
+			return None
+
+
+		if self.num%5 != 0:
+			oldrow = 1 + int(self.num/5)
+		else:
+			oldrow = int(self.num/5)
+
+		if num%5 != 0:
+			row = 1 + int(num/5)
+		else:
+			row = int(num/5)
+
+		print("old[%d], new[%d]" %(oldrow, row))
+
+		if num > self.num: 
+			for i in xrange(oldrow, row ):#增加缺少的
+				print("i[%d]" %(i))
+				start = 0
+				if i != oldrow or self.num == 0: #此行不用增加
+					print("new line[%d]" %(i))
+					self.gridLayout[i] = QtGui.QGridLayout()
+					self.gridLayout[i].setObjectName(_fromUtf8("gridLayout_" + str(i)))
+					self.label[i] = QtGui.QLabel(self)
+					self.label[i].setObjectName(_fromUtf8("lable_" + str(i)))
+					self.label[i].setText(_fromUtf8(str(i * 5 + 1) + "-" + str(i  * 5 + 5) + "关"))
+					self.gridLayout[i].addWidget(self.label[i], 0, 0, 1, 1)
+				else:
+					start = 5 - self.num%5
+
+
+				for j in xrange(start,5):
+					self.LevelEdit[i * 5 + j] = QtGui.QLineEdit(self)
+					self.LevelEdit[i * 5 + j].setText("123")
+					self.LevelEdit[i * 5 + j].setObjectName(_fromUtf8("Level_" + str(j) +"_Edit"))
+					self.gridLayout[i].addWidget(self.LevelEdit[i * 5 + j], 0, j + 1, 1, 1)
+
+				if i != oldrow or self.num == 0:
+					self.ui.verticalLayout.addLayout(self.gridLayout[i])
+
+		self.num = num
+
 
 	def reset(self):
 		self.ui.TimeLimitLine.setText('60')
